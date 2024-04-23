12d0acd77cc0dd5c17a5fbd4aca658a231b6d86c
krisztiannagy <krnagy@csumb.edu>
2024-04-23 12:29:00 -0700

    started part 3

--- src/main/java/org/main/Monsters/Imp.java
+++ src/main/java/org/main/Monsters/Imp.java
@@ -1,10 +1,22 @@
 package org.main.Monsters;
 
+import org.main.Abilities.MeleeAttack;
+
 import java.util.HashMap;
 
 public class Imp extends Monster {
+    HashMap<String, Integer> stats = new HashMap<String,Integer>();
     public Imp(Integer maxHP, Integer xp, HashMap<String, Integer> items){
         super(maxHP,xp,items);
+        Integer maxStr = 15;
+        Integer maxDef = 6;
+        Integer maxAgi = 3;
+
+        stats.put("str",super.getAttribute(str,maxStr));
+        stats.put("def",super.getAttribute(def,maxDef));
+        stats.put("agi",super.getAttribute(agi,maxAgi));
+
+        attack = new MeleeAttack(this);
     }
 
     @Override
--- src/main/java/org/main/Monsters/Monster.java
+++ src/main/java/org/main/Monsters/Monster.java
@@ -1,13 +1,21 @@
 package org.main.Monsters;
 
+import org.main.Abilities.Attack;
+
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.Random;
 
 public class Monster {
     private Integer hp;
     private Integer xp;
     private Integer maxHP;
+    Integer agi = 10;
+    Integer def = 10;
+    Integer str = 10;
+    Integer atk = 10;
     private HashMap<String, Integer> items;
+    Attack attack;
 
     public Monster(Integer maxHP, Integer xp, HashMap<String,Integer> items){
         this.maxHP = maxHP;
@@ -65,4 +73,43 @@ public class Monster {
     public int hashCode() {
         return Objects.hash(hp, xp, maxHP, items);
     }
+
+    /**
+     * Returns a value between max and min.
+     * @param min the smaller value passed in.
+     * @param max the larger value passed in.
+     * @return the value between max and min.
+     */
+    Integer getAttribute(Integer min, Integer max){
+        Random rand = new Random();
+        if (min>max){
+            Integer temp = min;
+            min = max;
+            max = temp;
+        }
+
+        return rand.nextInt(max-min) + min;
+    }
+
+    /** Given an amount of damage, removes it from the creature's health.
+     *  If the creature dies, returns false.
+     *
+     * @param damage the amount of damage taken
+     * @return true if the poor lil guy is still alive, or else false.
+     */
+    boolean takeDamage(Integer damage){
+        if (damage > 0){
+            if (hp-damage <= 0){
+                System.out.printf("Oh no! The creature has perished!%n");
+                hp = 0;
+            }
+            else{
+                hp -= damage;
+            }
+        }
+        System.out.println(this.toString());
+        return(hp>0);
+    }
+
+    
 }
